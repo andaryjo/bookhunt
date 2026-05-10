380c26523807561d4fe4a4c360fe2e5ee5e5fb63
Andary <dev@andary.de>
2026-05-10 23:19:33 +0200

    update summary

--- public/js/app.js
+++ public/js/app.js
@@ -182,9 +182,9 @@ function getDistance(lat1, lon1, lat2, lon2) {
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos((lat1 * Math.PI) / 180) *
-    Math.cos((lat2 * Math.PI) / 180) *
-    Math.sin(dLon / 2) *
-    Math.sin(dLon / 2);
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   return R * c;
 }
@@ -207,7 +207,7 @@ function showRecentBooks() {
   if (searchList) searchList.innerHTML = "";
 
   if (startPageSummary) {
-    startPageSummary.innerHTML = `Find <strong>${booksData.length}</strong> books in <strong>${bookshelves.length}</strong> public bookshelves`;
+    startPageSummary.innerHTML = `Discover <strong>${booksData.length}</strong> free books in <strong>${bookshelves.length}</strong> public bookshelves`;
     startPageSummary.classList.remove("hidden");
   }
 
